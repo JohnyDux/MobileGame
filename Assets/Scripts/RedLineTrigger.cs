ab354395a960752b0d232ff908df2ff683c77881
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-04-10 04:22:42 +0100

    Basic Game Mechanics Concluded
    The stacking of the bricks, the menus, the points ui and the main colliders are done

--- Assets/Scripts/RedLineTrigger.cs
+++ Assets/Scripts/RedLineTrigger.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class RedLineTrigger : MonoBehaviour
 {
     public GameObject WinCanvas;
+
+    public GameObject BrickNum;
+    public GameObject BrickImage;
     void Start()
     {
         WinCanvas.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Brick" )
         {
+            BrickImage.SetActive(false);
+            BrickNum.SetActive(false);
+
             WinCanvas.SetActive(true);
             Time.timeScale = 0.0f;
         }
