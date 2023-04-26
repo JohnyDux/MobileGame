57c7935ffe6e0bf2e6c6c58026175d58ce726a6c
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-04-26 13:46:50 +0100

    Architect Animation

---
+++ Assets/Scripts/ArchitectAttack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchitectAttack : MonoBehaviour
+{
+    public int TimesHitPlayer;
+    public Animator anim;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TimesHitPlayer+=1;
+            anim.SetBool("Attack", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+}
