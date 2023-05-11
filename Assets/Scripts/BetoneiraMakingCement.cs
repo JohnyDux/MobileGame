5e66a6b5d69142bf08e4b67c62e077f504c6b75e
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-05-11 05:13:42 +0100

    Crafting Completed

--- Assets/Scripts/BetoneiraMakingCement.cs
+++ Assets/Scripts/BetoneiraMakingCement.cs
@@ -6,6 +6,14 @@ public class BetoneiraMakingCement : MonoBehaviour
 {
     public Animator anim;
     public PlayerMovement player;
+    float lastWaterCount;
+    float lastSandCount;
+
+    private void Start()
+    {
+        lastWaterCount = 0;
+        lastSandCount = 0;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
@@ -28,10 +36,20 @@ public class BetoneiraMakingCement : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && player.cementCount <= 100)
         {
-            player.cementCount = player.cementCount + 30f;
+            if(player.cementCount + 30f > 100f)
+            {
+                player.cementCount = 100f;
+            }
+            else
+            {
+                player.cementCount = player.cementCount + 30;
+            }
+            
             anim.SetBool("MakeCement", false);
+            lastWaterCount = player.waterCount;
+            lastSandCount = player.sandCount;
         }
     }
 }
